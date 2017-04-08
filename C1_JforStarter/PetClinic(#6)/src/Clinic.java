9d0dea460c150ac0995e682daa9910e57f8cb675
Коваленко Константин <Phelixsky96@gmail.com>
2017-04-08 11:20:29 +0300

    Create Clinic
    Create project from lesson 6 Pet Clinic

---
+++ C1_JforStarter/PetClinic(#6)/src/Clinic.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Phoelix on 29.03.2017.
+ */
+public class Clinic {
+    /*
+     *Create Clinic
+     *@param NumOfClients - max 
+     */
+    public void Clinic(Integer NumOfClients) {
+
+
+    }
+}
